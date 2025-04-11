29ff7dcd3d2d7fbcb5721398a26f6b56d3b7f394
GTRrb26 <prathmeshkumavat000@gmail.com>
2025-04-11 21:12:32 +0530

    test for commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">Mini-CRM</a>
+            <a class="navbar-brand" href="{{ url('/') }}">Mini-CRM for Employees and Companies</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
